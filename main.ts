dbeca1deeee6e3c63ce110a69eee7e5632137d60
Finn Romaneessen <finn.romaneessen@tum.de>
2024-11-29 15:13:26 +0100

    Basic Sankey functionality

--- main.ts
+++ main.ts
@@ -1,134 +1,187 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import * as d3san from 'd3-sankey';
+import * as d3 from 'd3';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface Data {
+    nodes: d3san.SankeyNodeMinimal<any, any>[];
+    links: { source: string, target: string, value: number }[]
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface SNodeExtra {
+    nodeId: number;
+    name: string;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+interface SLinkExtra {
+    source: number;
+    target: number;
+    value: number;
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+type SNode = d3san.SankeyNode<SNodeExtra, SLinkExtra>;
+type SLink = d3san.SankeyLink<SNodeExtra, SLinkExtra>;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+interface DAG {
+    nodes: SNode[];
+    links: SLink[];
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+//TODO More efficient solution
+function mapNodeValues(data: DAG): Map<string, number> {
+    let kv = new Map<string, number>;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    data.nodes.forEach((node) => {
+        let input = 0;
+        let output = 0;
+        data.links.forEach((link) => {
+            if (link.target == node.nodeId) {
+                input += link.value;
+            }
+            if (link.source == node.nodeId) {
+                output += link.value;
+            }
+        });
+        kv.set(node.name, Math.max(input, output));
+    });
+    return kv;
+}
 
-	display(): void {
-		const {containerEl} = this;
+export default class SankeyPlugin extends Plugin {
+    dimensions = {
+        height: 600,
+        width: 900,
+        margins: 10
+    }
 
-		containerEl.empty();
+    async onload() {
+        this.registerMarkdownCodeBlockProcessor('sankey', (source, el, ctx) => {
+            const sankeyData: DAG = { nodes: [], links: [] };
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+            const staticData2: DAG = {
+                nodes: [{
+                    nodeId: 0,
+                    name: "node0"
+                }, {
+                    nodeId: 1,
+                    name: "node1"
+                }, {
+                    nodeId: 2,
+                    name: "node2"
+                }, {
+                    nodeId: 3,
+                    name: "node3"
+                }, {
+                    nodeId: 4,
+                    name: "node4"
+                }], links: [{
+                    source: 0,
+                    target: 4,
+                    value: 1,
+                }, {
+                    source: 0,
+                    target: 1,
+                    value: 4,
+                }, {
+                    source: 1,
+                    target: 2,
+                    value: 2,
+                }, {
+                    source: 1,
+                    target: 3,
+                    value: 2,
+                }, {
+                    source: 4,
+                    target: 3,
+                    value: 1,
+                }]
+            };
+
+            const rows = source.split('\n').filter((row) => row.length > 0);
+            const nodeMap = new Map<string, number>;
+            let nodeCnt = 0;
+
+            rows.forEach((row) => {
+                const cols = row.split(',');
+
+                if (cols.length != 3) {
+                    return;
+                }
+
+                if (!nodeMap.has(cols[0])) { nodeMap.set(cols[0], nodeCnt++); }
+                if (!nodeMap.has(cols[1])) { nodeMap.set(cols[1], nodeCnt++); }
+
+                sankeyData.links.push({ source: nodeMap.get(cols[0])!, target: nodeMap.get(cols[1])!, value: +cols[2] });
+
+                const nodesList = sankeyData.nodes.map((n) => n.name);
+                if (!nodesList.includes(cols[0])) {
+                    sankeyData.nodes.push({ name: cols[0], nodeId: nodeMap.get(cols[0])! });
+                }
+                if (!nodesList.includes(cols[1])) {
+                    sankeyData.nodes.push({ name: cols[1], nodeId: nodeMap.get(cols[0])! });
+                }
+            });
+
+            const valueMap = mapNodeValues(sankeyData);
+
+            const sankeyD = d3san.sankey()
+                .nodes(sankeyData.nodes)
+                .links(sankeyData.links)
+                .nodeAlign(d3san.sankeyJustify)
+                .nodeWidth(100)
+                .extent([
+                    [this.dimensions.margins, this.dimensions.margins],
+                    [
+                        this.dimensions.width - this.dimensions.margins * 2,
+                        this.dimensions.height - this.dimensions.margins * 2
+                    ]
+                ]);
+
+            sankeyD(sankeyData);
+
+            const svg = d3.create('svg')
+                .attr("height", this.dimensions.height)
+                .attr("width", this.dimensions.width)
+                .attr("overflow", "visible")
+                .style('background', 'white');
+
+            const nodes = svg
+                .append("g")
+                .selectAll("rect")
+                .data(sankeyData.nodes)
+                .join("rect")
+                .attr("x", (d) => d.x0!)
+                .attr("y", (d) => d.y0!)
+                .attr("fill", 'dodgerblue')
+                .attr("height", (d) => d.y1! - d.y0!)
+                .attr("width", (d) => d.x1! - d.x0!);
+
+            const links = svg
+                .append("g")
+                .attr("fill", "none")
+                .attr("stroke", "black")
+                .attr("stroke-opacity", 0.2)
+                .selectAll("path")
+                .data(sankeyData.links)
+                .join("path")
+                .attr("d", d3san.sankeyLinkHorizontal())
+                .attr("stroke-width", (d) => d.width!);
+
+            svg.append("g")
+                .selectAll()
+                .data(sankeyData.nodes)
+                .join("text")
+                .attr("x", d => d.x0! < this.dimensions.width / 2 ? d.x1! + 6 : d.x0! - 6)
+                .attr("y", d => (d.y1! + d.y0!) / 2)
+                .attr("dy", "0.35em")
+                .attr("text-anchor", d => d.x0! < this.dimensions.width / 2 ? "start" : "end")
+                .text(d => `${d.name}: ${valueMap.get(d.name)}`);
+
+            el.appendChild(svg.node()!);
+        });
+    }
+
+    onunload() {
+
+    }
+
 }
